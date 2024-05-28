bcaf9413ed704a7d3976be3210a1ccfdd9b7a663
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2024-05-29 00:10:35 +0330

    Facade pattern for codeGenerator

---
+++ src/main/java/codeGenerator/CodeGeneratorFacade.java
@@ -0,0 +1,18 @@
+package codeGenerator;
+
+import scanner.token.Token;
+
+public class CodeGeneratorFacade {
+    private CodeGenerator codeGenerator;
+    public CodeGeneratorFacade() {
+        codeGenerator = new CodeGenerator();
+    }
+
+    public void semanticFunction(int func, Token next) {
+        codeGenerator.semanticFunction(func, next);
+    }
+
+    public void printMemory() {
+        codeGenerator.printMemory();
+    }
+}
